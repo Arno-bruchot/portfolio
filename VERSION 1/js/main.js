687ef8bcb1130ec36529407c73266320d8682886
Arno-bruchot <arno.bruchot2@lyceeconnecte.fr>
2025-06-18 21:33:28 +0200

    Add files via upload

---
+++ VERSION 1/js/main.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('nav ul li a');
+  const sections = document.querySelectorAll('main section');
+
+  function showSection(id) {
+    sections.forEach(section => {
+      if (section.id === id) {
+        section.classList.remove('hidden-section');
+      } else {
+        section.classList.add('hidden-section');
+      }
+    });
+  }
+
+  links.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+
+      // Mise à jour classe active sur le menu
+      links.forEach(l => l.classList.remove('active'));
+      link.classList.add('active');
+
+      const targetId = link.getAttribute('href').substring(1);
+      showSection(targetId);
+    });
+  });
+
+  // Affichage initial (intro visible)
+  showSection('intro');
+  const fills = document.querySelectorAll('.progress-bar-fill');
+
+const animateBars = () => {
+  fills.forEach(bar => {
+    const target = bar.getAttribute('data-fill');
+    bar.style.width = target;
+  });
+};
+
+// Tu peux appeler l'animation juste après DOMContentLoaded
+setTimeout(animateBars, 400);
+
+});
